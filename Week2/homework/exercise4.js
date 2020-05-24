61216cd693a692c409b40014a1577b18b7fa77e0
mucahit04 <57506115+mucahit04@users.noreply.github.com>
2020-05-24 15:55:48 +0200

    Added exercise 4(aggregate functions)

--- Week2/homework/exercise4.js
+++ Week2/homework/exercise4.js
@@ -2,62 +2,73 @@ const util = require("util");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "hyfuser",
-  password: "hyfpassword",
-  database: "library",
+	host: "localhost",
+	user: "hyfuser",
+	password: "hyfpassword",
+	database: "library",
 });
 
-const execQuery = util.promisify(
-  connection.query.bind(connection)
-);
+const execQuery = util.promisify(connection.query.bind(connection));
 
 async function seedDatabase() {
-  //Write a query that prints names of all Authors and their corresponding Collaborators.
-  const authors_collaborators =
-    "SELECT A1.Author_name, A2.author_name as Collaborator from authors as A1 INNER JOIN authors as A2 ON A1.collaborator = A2.author_no;";
+	//1- All research papers and the number of authors that wrote that paper.
+	const rPapers_collaborators = `
+  SELECT rp.paper_title, 
+  COUNT(DISTINCT r.author_id) 
+  AS 'Authors' FROM relation_table
+  r RIGHT JOIN Research_Papers 
+  rp ON rp.paper_id = r.paper_id 
+  GROUP BY rp.paper_id;`;
 
-  const authors_published_papers =
-    "SELECT A.Author_name, P.paper_title AS Paper_Title from authors AS A INNER JOIN relation_table as R ON A.author_no=R.author_id INNER JOIN Research_Papers AS P ON R.paper_id=P.paper_id;";
+	//2- Sum of the research papers published by all female authors.
+	const sumPapersByFemale = `
+    SELECT a.gender , COUNT(r.paper_id) 
+    FROM relation_table r 
+    INNER JOIN Authors a 
+    on a.author_no = r.author_id 
+    WHERE a.gender ='f';
+    `;
 
-  connection.connect();
+	//3- Average of the h-index of all authors per university.
+	const avrgHindexOfUniversity = `
+    SELECT university, AVG(h_index) AS H_index_Average
+    FROM Authors GROUP BY (university)`;
 
-  try {
-    await connection.query(
-      authors_collaborators,
-      function (error, results, fields) {
-        if (error) throw error;
-        Object.keys(results).forEach((key) => {
-          var row = results[key];
-          console.log(
-            row.Author_name,
-            row.Collaborator
-          );
-        });
-        console.log(
-          "|||||||||||||||||||||||||||||||||||||"
-        );
-      }
-    );
+	//4- Sum of the research papers of the authors per university.
+	const SumOfPapersPerUniversity = `
+    SELECT A.university, 
+    COUNT(DISTINCT AP.paper_id) 
+    AS Sum_Of_Papers 
+    FROM Relation_Table 
+    AS AP JOIN Authors 
+    AS A ON AP.author_id = A.author_no 
+    GROUP BY(A.university)`;
 
-    await connection.query(
-      authors_published_papers,
-      function (error, results, fields) {
-        if (error) throw error;
-        Object.keys(results).forEach((key) => {
-          var row = results[key];
-          console.log(
-            row.Author_name,
-            row.Paper_Title
-          );
-        });
-      }
-    );
-  } catch (error) {
-    console.error(error);
-    connection.end();
-  } finally {
-    connection.end();
-  }
+	//5- Minimum and maximum of the h-index of all authors per university.
+	const MinMaxHindexOfUniversity = `
+    SELECT university, 
+    MIN(h_index), 
+    MAX(h_index)
+    FROM Authors
+    GROUP BY university`;
+
+	connection.connect();
+	try {
+		console.log("All research papers and the number of authors that wrote that paper");
+		console.log(await execQuery(rPapers_collaborators));
+		console.log("Sum of the research papers published by all female authors");
+		console.log(await execQuery(sumPapersByFemale));
+		console.log("Average of the h-index of all authors per university");
+		console.log(await execQuery(avrgHindexOfUniversity));
+		console.log("Sum of the research papers of the authors per university");
+		console.log(await execQuery(SumOfPapersPerUniversity));
+		console.log("Minimum and maximum of the h-index of all authors per university");
+		console.log(await execQuery(MinMaxHindexOfUniversity));
+	} catch (error) {
+		console.error(error);
+	} finally {
+		connection.end();
+	}
 }
+
 seedDatabase();
