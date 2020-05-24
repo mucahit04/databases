9d7e0698f61eed28e2989e25523952edb79edb80
mucahit04 <57506115+mucahit04@users.noreply.github.com>
2020-05-24 14:43:11 +0200

    used enum instead of text and tidied the queries

--- Week2/homework/exercise1-keys.js
+++ Week2/homework/exercise1-keys.js
@@ -2,38 +2,45 @@ var mysql = require("mysql");
 const util = require("util");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "hyfuser",
-  password: "hyfpassword",
-  database: "userdb",
+	host: "localhost",
+	user: "hyfuser",
+	password: "hyfpassword",
+	database: "userdb",
 });
 
 const execQuery = util.promisify(connection.query.bind(connection));
 
 async function seedDatabase() {
-  const create_authors_table =
-    "CREATE TABLE authors(author_no int PRIMARY KEY, author_name text, university text, date_of_birth date, h_index int, gender text);";
+	const create_authors_table = `CREATE TABLE authors(
+    author_no int PRIMARY KEY,
+    author_name text, 
+    university text,
+    date_of_birth date, 
+    h_index int, 
+    gender enum)`;
 
-  const fk_Collaborator =
-    "ALTER TABLE authors ADD collaborator INT, ADD CONSTRAINT fk_Collaborator FOREIGN KEY (collaborator) REFERENCES authors(author_no);";
+	const fk_Collaborator = `ALTER TABLE authors 
+    ADD collaborator INT, 
+    ADD CONSTRAINT 
+    fk_Collaborator 
+    FOREIGN KEY (collaborator) 
+    REFERENCES authors(author_no)`;
 
-  connection.connect();
+	connection.connect();
 
-  try {
-    await Promise.all[
-      (execQuery("DROP DATABASE IF EXISTS library"),
-      execQuery("CREATE DATABASE library"))
-    ];
+	try {
+		await Promise.all[
+			(execQuery("DROP DATABASE IF EXISTS library"), execQuery("CREATE DATABASE library"))
+		];
 
-    await Promise.all[
-      (execQuery("USE library"),
-      (execQuery(create_authors_table), execQuery(fk_Collaborator)))
-    ];
-  } catch (error) {
-    console.error(error);
-  }
+		await Promise.all[
+			(execQuery("USE library"), (execQuery(create_authors_table), execQuery(fk_Collaborator)))
+		];
+	} catch (error) {
+		console.error(error);
+	}
 
-  connection.end();
+	connection.end();
 }
 
 seedDatabase();
